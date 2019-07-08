92966b047bc73f7a677d16ab5a457bbf4d475e52
Luiz Batanero <luizbatanero@gmail.com>
2019-07-08 19:28:19 -0300

    Handle duplicated products

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
       { extensions: ['.jsx', '.js'] }
     ],
     'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
     'no-console': ['error', { allow: ['tron' ] }]
   },
 };
