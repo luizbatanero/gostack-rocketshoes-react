8e60e9241f78bc55be27db8cdbfd4f5ccf46acd3
Luiz Batanero <luizbatanero@gmail.com>
2019-07-08 18:56:50 -0300

    Setup Redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 import Header from './components/Header';
 
+import store from './store';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
