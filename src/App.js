62aeaae774a585efefeb80195964d7f97ec44c48
Luiz Batanero <luizbatanero@gmail.com>
2019-07-08 17:52:29 -0300

    Add Header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
       <GlobalStyle />
     </BrowserRouter>
---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/rocketshoes.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="RocketShoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>My cart</strong>
+          <span>3 itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
+    </Container>
+  );
+}
