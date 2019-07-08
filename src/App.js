f68a0f699e4b7bd2b61a79d2c5d76d6b76e09eab
Luiz Batanero <luizbatanero@gmail.com>
2019-07-08 17:44:10 -0300

    Add GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
