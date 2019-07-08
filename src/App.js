b40f97f62407588b044fa4e89fac26efcea483af
Luiz Batanero <luizbatanero@gmail.com>
2019-07-08 17:34:18 -0300

    Configure routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Home() {
+  return <h1>Home</h1>;
+}
