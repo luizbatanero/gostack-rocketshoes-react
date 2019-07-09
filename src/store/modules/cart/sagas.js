3e831c8437da7e45fefefa3d11332731ae037fd2
Luiz Batanero <luizbatanero@gmail.com>
2019-07-08 23:08:13 -0300

    Check stock when adding products

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -9,9 +9,19 @@ function* addToCart({ id }) {
     state.cart.find(p => p.id === id)
   );
 
-  if (productExists) {
-    const amount = productExists.amount + 1;
+  const stock = yield call(api.get, `/stock/${id}`);
 
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('erro');
+    return;
+  }
+
+  if (productExists) {
     yield put(updateAmount(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
