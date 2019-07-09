f0c8ceade9a8ecd6360aa5ba0b1b6df47ec2ae51
Luiz Batanero <luizbatanero@gmail.com>
2019-07-08 22:54:37 -0300

    Setup redux-saga

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -25,10 +25,10 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   };
 
   render() {
@@ -45,7 +45,7 @@ class Home extends Component {
 
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" />{' '}
