057a43e7a12f35ce3612f9b414c40fbdc97e17c8
Luiz Batanero <luizbatanero@gmail.com>
2019-07-09 00:04:18 -0300

    Add history service

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
@@ -9,17 +9,18 @@ import GlobalStyle from './styles/global';
 import Routes from './routes';
 import Header from './components/Header';
 
+import history from './services/history';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
-        <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+        <ToastContainer />
+      </Router>
     </Provider>
   );
 }
